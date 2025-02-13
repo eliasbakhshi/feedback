87218589cbd2b4dc8354fd79edd122f828876452
Samuel Stenfelt <sasn23@student.bth.se>
2025-02-13 13:02:36 +0100

    cors fix

--- backend/Program.cs
+++ backend/Program.cs
@@ -9,10 +9,9 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://localhost:5173", "http://127.0.0.1:5173")
+                                policy.AllowAnyOrigin()
                                 .AllowAnyMethod()
-                                .AllowAnyHeader()
-                                .AllowCredentials();
+                                .AllowAnyHeader();
                       });
 });
 
