1b49e4a7c1099a44d9a8f2634271e17497ec1946
Sandy Nguyen <Sandynguyen701@yahoo.se>
2025-02-14 10:34:18 +0100

    Removed username

--- backend/Models/RegistrationModel.cs
+++ backend/Models/RegistrationModel.cs
@@ -7,7 +7,6 @@ namespace backend.Models
 {
     public class RegistrationModel
     {
-        public string? Username { get; set; }
         public string? FullName { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
