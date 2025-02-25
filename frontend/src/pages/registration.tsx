f733450ed771eb7b65c34c72285ccf732cf6bb92
Sandy Nguyen <Sandynguyen701@yahoo.se>
2025-02-25 08:27:39 +0100

    cahgned button design

--- frontend/src/pages/registration.tsx
+++ frontend/src/pages/registration.tsx
@@ -82,7 +82,7 @@ const Registration = () => {
             onChange={(e) => setPassword(e.target.value)}
             className="w-full px-4 py-2 mt-1 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
           />
-          <button className="w-20 py-2 mt-4 text-white bg-red-600 rounded-lg hover:bg-red-700 transition duration-300" onClick={handleRegistration} disabled={isLoading}>
+          <button className="w-24 py-2 mt-4 text-white bg-red-600 rounded-lg hover:bg-red-700 transition duration-300" onClick={handleRegistration} disabled={isLoading}>
             Registrera
           </button>
         </div>
