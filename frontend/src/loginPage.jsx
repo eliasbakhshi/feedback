e2589fead5066ff245ec0f5c2c0089f95833acf1
Linn <lich23@student.bth.se>
2025-02-26 13:13:19 +0100

    Uppdatera registration

--- frontend/src/loginPage.jsx
+++ frontend/src/loginPage.jsx
@@ -1,30 +1,46 @@
 import { userState } from "react"; // This is a hook that allows us to store the user's state
 import { useNavigate } from "react-router-dom"; // This is a hook that allows us to navigate to different pages
+import { loginUser } from "../store/authSlice";
+import { useDispatch, useSelector } from "react-redux"; // This is a hook that allows us to dispatch actions
+
+
 
 // A fake user object that we will use to simulate a user logging in
-const fakeUser = {
+/*const fakeUser = {
   email: "test@example.com", // The user's email
   password: "password", // The user's password
-};
+};*/
 
 const LoginPage = () => {
     const [email, setEmail] = useState(""); // The user's email
     const [password, setPassword] = useState(""); // The user's password
-    const [error, setError] = useState(""); // The error message to display to the user
+    const dispatch = useDispatch(); // The dispatch function that we will use to dispatch actions
     const navigate = useNavigate(); // The navigate function that we will use to navigate to different pages
+    
+    const {loading, error } = useSelector((state) => state.auth);
 
     const handleLogin = (e) => {
         e.preventDefault();
-        setError(""); // Clear the error message
+        dispatch(loginUser({ email, password }))
+            .unwrap()
+            .then(() => {
+                navigate("/"); // Skicka användaren till startsidan efter inloggning
+            })
+            .catch((err) => {
+                console.error("Login failed:", err);
+            });
+    };
+
+
 
         // Simulate a login request
-        if (email === fakeUser.email && password === fakeUser.password) {
+        /*if (email === fakeUser.email && password === fakeUser.password) {
             localStorage.setItem("token", "fakeToken"); // Save the token to local storage
             navigate("/"); // Navigate to the home page
         } else {
             setError("Fel e-post eller lösenord"); // Set the error message
         }
-    };
+    };*/
 
     return (
         <div> {/* The login form */}
