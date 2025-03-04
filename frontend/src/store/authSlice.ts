d58ae782edf330e3e5da90769090b53815803de4
Sandy Nguyen <Sandynguyen701@yahoo.se>
2025-03-04 17:44:21 +0100

    restored authSlice

--- frontend/src/store/authSlice.ts
+++ frontend/src/store/authSlice.ts
@@ -1,31 +1,52 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// Hämta userInfo från localStorage och hantera potentiella fel
-const getUserFromStorage = () => {
-  try {
-    const storedUser = localStorage.getItem("userInfo");
-    return storedUser && storedUser !== "undefined" ? JSON.parse(storedUser) : null;
-  } catch (error) {
-    console.error("Error parsing userInfo from localStorage:", error);
-    return null;
-  }
-};
+// TODO: Get the current expirationTime and set it again if there is not any other remember option
 
-const initialState = {
-  userInfo: getUserFromStorage(),
-};
+
+const initialState = (() => {
+  const userInfo = localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null;
+  const expirationTime = localStorage.getItem("expirationTime")
+    ? JSON.parse(localStorage.getItem("expirationTime"))
+    : null;
+
+  // Check if the expirationTime is less than the current time
+  if (expirationTime) {
+    const expirationDate = new Date(expirationTime);
+    if (new Date().getTime() > expirationDate.getTime()) {
+      localStorage.removeItem("userInfo");
+      localStorage.removeItem("expirationTime");
+      return {
+        userInfo: null,
+        expirationTime: null,
+      };
+    }
+  }
+
+  return {
+    userInfo,
+    expirationTime,
+  };
+})();
 
 export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
     setCredentials: (state, action) => {
+      const duration = action.payload.remember ? 30 : 1;
+      const expirationTime =
+        new Date().getTime() + 24 * 60 * 60 * 1000 * duration;
       state.userInfo = action.payload.user;
+      state.expirationTime = expirationTime;
       localStorage.setItem("userInfo", JSON.stringify(action.payload.user));
+      localStorage.setItem("expirationTime", expirationTime);
     },
     removeCredentials: (state) => {
       state.userInfo = null;
       localStorage.removeItem("userInfo");
+      localStorage.removeItem("expirationTime");
     },
   },
 });
