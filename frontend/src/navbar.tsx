6a93c22ddac9ce02cfea4e9d37a29e651457bf37
Sandy Nguyen <Sandynguyen701@yahoo.se>
2025-03-06 15:09:10 +0100

    changed home icon

--- frontend/src/navbar.tsx
+++ frontend/src/navbar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { LiaChevronLeftSolid, LiaChevronRightSolid, LiaUser, LiaFolderSolid , LiaFolderPlusSolid } from "react-icons/lia";
+import { LiaChevronLeftSolid, LiaChevronRightSolid, LiaUser, LiaHomeSolid  , LiaFolderPlusSolid } from "react-icons/lia";
 import {Link} from "react-router-dom";
 import logo from "/TV_logo_symbol_rgb_rod.png";
 
@@ -8,18 +8,19 @@ const Sidebar = () => {
 
   return (
     <div className={`h-screen text-gray-800 flex flex-col p-4 transition-all ${collapsed ? "w-16" : "w-48"} border-r border-gray-600`}>
+        <button onClick={() => setCollapsed(!collapsed)} className="mb-4 p-2 rounded hover:bg-gray-300">
+        {collapsed ? <LiaChevronRightSolid /> : <LiaChevronLeftSolid /> }
+        </button>
         <img 
             src={logo} 
             alt="logo" 
             className={`transition-all duration-300 w-16 mb-4 p-2 rounded`} 
         />
-      <button onClick={() => setCollapsed(!collapsed)} className="mb-4 p-2 rounded hover:bg-gray-300">
-        {collapsed ? <LiaChevronRightSolid /> : <LiaChevronLeftSolid /> }
-      </button>
+      
 
       <nav className="flex flex-col gap-4">
         <Link to="/" className="p-2 rounded hover:bg-gray-300 flex items-center gap-2">
-          <LiaFolderSolid />
+          <LiaHomeSolid  />
           {!collapsed && <p>Hem</p>}
         </Link>
         <Link to="/" className="p-2 rounded hover:bg-gray-300 flex items-center gap-2">
