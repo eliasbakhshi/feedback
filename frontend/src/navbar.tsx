0444f98c89fa433ce92230e8bba2e782acfafd29
Sandy Nguyen <Sandynguyen701@yahoo.se>
2025-03-07 12:07:44 +0100

    made the icons bigger

--- frontend/src/navbar.tsx
+++ frontend/src/navbar.tsx
@@ -7,28 +7,29 @@ const Sidebar = () => {
   const [collapsed, setCollapsed] = useState(false);
 
   return (
-    <div className={`h-screen text-gray-800 flex flex-col p-4 transition-all ${collapsed ? "w-16" : "w-48"} border-r border-gray-600`}>
-        <button onClick={() => setCollapsed(!collapsed)} className="mb-4 p-2 rounded hover:bg-gray-300">
-        {collapsed ? <LiaChevronRightSolid /> : <LiaChevronLeftSolid /> }
-        </button>
-        <img 
+    <div className={`h-screen text-gray-800 flex flex-col p-4 transition-all ${collapsed ? "w-20" : "w-60"} border-r border-gray-600`}>
+      <img 
             src={logo} 
             alt="logo" 
-            className={`transition-all duration-300 w-16 mb-4 p-2 rounded`} 
+            className={`transition-all duration-300 w-20 mb-4 p-2 rounded`} 
         />
+        <button onClick={() => setCollapsed(!collapsed)} className="mb-4 p-2 rounded hover:bg-gray-300">
+        {collapsed ? <LiaChevronRightSolid size={28} /> : <LiaChevronLeftSolid size={28} /> }
+        </button>
+        
       
 
-      <nav className="flex flex-col gap-4">
+      <nav className="flex flex-col gap-6">
         <Link to="/" className="p-2 rounded hover:bg-gray-300 flex items-center gap-2">
-          <LiaHomeSolid  />
+          <LiaHomeSolid size={28} />
           {!collapsed && <p>Hem</p>}
         </Link>
         <Link to="/" className="p-2 rounded hover:bg-gray-300 flex items-center gap-2">
-          <LiaFolderPlusSolid />
+          <LiaFolderPlusSolid size={28} />
           {!collapsed && <p>Skapa Enkät</p>}
         </Link>
         <Link to="/account" className="p-2 rounded hover:bg-gray-300 flex items-center gap-2">
-          <LiaUser />
+          <LiaUser size={28} />
           {!collapsed && <p>Konto</p>}
         </Link>
       </nav>
