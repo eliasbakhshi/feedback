0c39867905139f16a055c91c0f7766e156c76b5a
Sandy Nguyen <Sandynguyen701@yahoo.se>
2025-03-12 16:14:57 +0100

    changed sidebar imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import NavigationBar from "./navbar.tsx"
 
 function App() {
   return (
     <>
       <ToastContainer />
-      <NavigationBar />
       <main>
         <Outlet />
       </main>
@@ -16,5 +14,3 @@ function App() {
 }
 
 export default App;
-
-
