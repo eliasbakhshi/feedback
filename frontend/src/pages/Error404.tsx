b08afafefe27ea11b563f7e27092664a7f63a173
Sandy Nguyen <Sandynguyen701@yahoo.se>
2025-03-13 09:39:05 +0100

    import sideba in app.tsx

--- frontend/src/pages/Error404.tsx
+++ frontend/src/pages/Error404.tsx
@@ -1,14 +1,12 @@
-import Sidebar from "../navbar";
+
 function Error404() {
   return (
     <>
-    <div className="flex h-screen w-full">
-      <Sidebar />
+
       <div className="flex h-full w-full flex-col items-center justify-center flex-grow">
         <h1 className="text-6xl font-bold text-gray-800">404</h1>
         <p className="text-xl text-gray-600">Oops! Page not found.</p>
       </div>
-    </div>
     </>
   );
 }
