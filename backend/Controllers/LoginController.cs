c3a7b19e6702f1c2124c69369d062a1d4741afc0
Pandy9 <andypersson2001@gmail.com>
2025-03-15 14:20:23 +0100

    Fixade formattering för måsvingarna

--- backend/Controllers/LoginController.cs
+++ backend/Controllers/LoginController.cs
@@ -10,30 +10,24 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace backend.Controllers
-{
+namespace backend.Controllers {
     [Route("api/login")]
-    public class LoginController : Controller
-    {
+    public class LoginController : Controller {
         DBManager dbManager = new DBManager();
 
         private readonly ILogger<LoginController> _logger;
 
-        public LoginController(ILogger<LoginController> logger)
-        {
+        public LoginController(ILogger<LoginController> logger) {
             _logger = logger;
         }
 
         [HttpPost]
-        public IActionResult Login([FromBody] LoginModel loginCredentials)
-        {
-            try
-            {
+        public IActionResult Login([FromBody] LoginModel loginCredentials) {
+            try {
                 int userID;
                 string role;
 
-                using (var db = dbManager.connect())
-                {
+                using (var db = dbManager.connect()) {
                     var query = @$"SELECT * FROM check_login_credentials('{loginCredentials.Email}', '{loginCredentials.Password}')";
                     var user = dbManager.select(db, query);
 
@@ -47,8 +41,7 @@ namespace backend.Controllers
                     role = userData["role"].ToString();
                 }
 
-                using (var db = dbManager.connect())
-                {
+                using (var db = dbManager.connect()) {
                     string token = createToken();
 
                     string saveTokenQuery = @$"CALL add_token('{token}', {userID})";
@@ -56,31 +49,27 @@ namespace backend.Controllers
                 }
 
                 string hashedToken;
-                using (var db = dbManager.connect())
-                {
+                using (var db = dbManager.connect()) {
                     var getTokenQuery = @$"SELECT * FROM get_hashed_token({userID})";
                     var result = dbManager.select(db, getTokenQuery);
 
                     hashedToken = result[0]["get_hashed_token"].ToString();
                 }
 
-                return Ok(new
-                {
+                return Ok(new {
                     Message = "Login successful",
                     Token = hashedToken,
                     UserId = userID,
                     Role = role
                 });
             }
-            catch (Exception ex)
-            {
+            catch (Exception ex) {
                 _logger.LogError(ex, "An error occurred when logging in.");
                 return StatusCode(StatusCodes.Status500InternalServerError, new { Message = "Failed to login user." });
             }
         }
 
-        public string createToken()
-        {
+        public string createToken() {
             byte[] tokenBytes = RandomNumberGenerator.GetBytes(32);
             return Convert.ToBase64String(tokenBytes);
         }
