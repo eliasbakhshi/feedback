6fc4051620bf20c27649bc16815438fb5d7331ab
Sandy Nguyen <Sandynguyen701@yahoo.se>
2025-03-20 17:08:27 +0100

    add link from register to login and vice versa

--- frontend/src/pages/auth/Registration.tsx
+++ frontend/src/pages/auth/Registration.tsx
@@ -98,6 +98,12 @@ const Registration = () => {
           <button className="w-24 py-2 mt-4 text-white bg-red-600 rounded-lg hover:bg-red-700 transition duration-300" onClick={handleRegistration} disabled={isLoading}>
             Registrera
           </button>
+          <p className="text-gray-700 text-sm mt-4 text-center">
+            Har du redan ett konto? {" "}
+            <a href="/login" className="text-red-500 hover:underline">
+              Logga in här
+            </a>
+          </p>
         </div>
       </div>
     </div>
